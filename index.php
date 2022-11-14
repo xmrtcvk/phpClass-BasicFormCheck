7ae447c325b540e26cc732198905c58a73976174
Mert Çevik <40180490+xmrtcvk@users.noreply.github.com>
2022-11-14 21:27:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<center>
+  <form  action="sonuc.php" method="post">
+    İsim : <input type="text" name="isim" > <br>
+    Yaş  :<input type="text" name="yas" > <br>
+    Tc   :<input type="number" name="tc"> <br>
+    Hobi :<input type="text" name="hobi" > <br>
+    Meslk:<input type="text" name="meslek" > <br>
+    <input type="submit" name="button" >
+  </form>
+</center>
---
+++ sonuc.php
@@ -0,0 +1,59 @@
+<?php
+
+    class formChecker{
+
+       protected $durum = false;
+       protected $mesaj= "Lütfen eksiksiz doldurunuz !";
+
+       protected $values = [];
+       protected $variables = [];
+       protected $lastVal = [];
+
+       function getValues(){
+
+         foreach ($_POST as $key => $value) {
+           array_push($this->variables, $value);
+           array_push($this->values, $key);
+        }
+
+         array_pop($this->variables);
+         array_pop($this->values);
+
+         $this->lastVal = array_combine($this->values,$this->variables);
+       }
+
+       function checkForm(){
+
+         foreach ($this->lastVal as $key => $value) {
+           if($value == ""){
+             return $this->mesaj;
+           }else {
+             $this->durum = true;
+           }
+         }
+       }
+
+       function getDurum(){
+         return $this->durum;
+       }
+
+       function addDb(){
+         if(self::getDurum() == true) {
+           echo "Her şey yolunda! <br>";
+
+           foreach ($this->lastVal as $key => $value) {
+             echo $key." = ".$value."<br>";
+           }
+         }
+       }
+    }
+
+    $kontrol = new formChecker;
+    $kontrol->getValues();
+    echo $kontrol->checkForm();
+    echo $kontrol->getDurum();
+    $kontrol->addDb();
+
+
+
+ ?>
